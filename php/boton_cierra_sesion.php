d6ff1b7b6ee5012917af26ed40a2a2f6e6a41f0c
jjaviergr1176 <jjaviergr1176@gmail.com>
2015-12-04 13:48:26 +0100

    boton cerrar sesion añadido

---
+++ php/boton_cierra_sesion.php
@@ -0,0 +1,26 @@
+<!--<script>
+//function hazte() {
+//    //document.getElementById("demo").style.color = "red";
+//    document.write("
+//    //<?php  
+//          header('Location : http://localhost/noticias/php/cerrar_sesion.php'); 
+//          print '<p>EEEEEEEEEEEEEEEEEEEE</p>'; 
+//    ?>//");
+//    
+//    
+//    //                  ?> +" ");
+//}
+</script>-->
+<!DOCTYPE html>
+<head>
+     <meta charset="utf-8">
+</head>
+<body>
+<script>
+</script>
+
+<button><a href="cerrar_sesion.php">Cerrar Sesión</a></button>
+</body>
+
+<!--<p id="demo" onclick="hazte()">Click me .</p>-->
+
